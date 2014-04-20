8143b3122c4c80d6ca4f90fb5f87c98538fcf544
Gonzalo Rodriguez <gonzalo89@ymail.com>
2014-04-20 16:21:45 -0300

    lectura de json

--- src/com/recorridaszo/recorridaszo/ManejadorBD.java
+++ src/com/recorridaszo/recorridaszo/ManejadorBD.java
@@ -1,6 +1,30 @@
 package com.recorridaszo.recorridaszo;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.util.Log;
+import android.widget.Toast;
+
 public class ManejadorBD {
+	String id;
+	String name;
+	InputStream is=null;
+	String result=null;
+	String line=null;
 
 	private static ManejadorBD INSTANCE = new ManejadorBD();
 
@@ -13,4 +37,46 @@ public class ManejadorBD {
 	public static ManejadorBD getInstance() {
 		return INSTANCE;
 	}
+
+	public void select() {
+		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+		nameValuePairs.add(new BasicNameValuePair("id", id));
+
+		try {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost("http://pruebazo.atwebpages.com/pruebajson.php");
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+			Log.e("pass 1", "connection success ");
+		} catch (Exception e) {
+			Log.e("Fail 1", e.toString());
+		}
+
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					is, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+			result = sb.toString();
+			Log.e("pass 2", "connection success ");
+		} catch (Exception e) {
+			Log.e("Fail 2", e.toString());
+		}
+
+		try {
+			JSONObject json_data = new JSONObject(result);
+			name = (json_data.getString("nombre"));
+			Log.e("pass 3", "connection success ");
+			Log.d("ZO", "nombre: "+name);
+		} catch (Exception e) {			
+			Log.e("Fail 3", e.toString());
+		}
+	}
+	
 }
